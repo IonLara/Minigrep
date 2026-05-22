62fe6b573cce92bf1fefedc97d6b79bbb8cba323
SebasMac <ionrodriguezlara@gmail.com>
2026-05-21 21:28:30 -0600

    Added Colored Crate and implemented colored output
    Now the program will return the lines found highlighting in green the queries

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use colored::Colorize;
 use std::env;
 use std::error::Error;
 use std::fs;
@@ -41,7 +42,6 @@ impl Config {
         let query = args[1].clone();
         let query = parse_query(query);
         let file_path = args[2].clone();
-        // let ignore_case = env::var("IGNORE_CASE").is_ok();
         let ignore_case = if args.len() < 4 {
             env::var("IGNORE_CASE").is_ok()
         } else {
@@ -56,13 +56,18 @@ impl Config {
     }
 }
 
+pub fn colorize(query: &str, line: &str) -> String {
+    line.replace(query, &query.green().to_string())
+}
+
 pub fn search<'a>(queries: Vec<String>, contents: &'a str) -> Vec<String> {
     let mut results = Vec::new();
 
     for query in queries {
         for (i, line) in contents.lines().enumerate() {
             if line.contains(&query) {
-                results.push(format!("{i} {line}"));
+                let colored = colorize(&query, line);
+                results.push(format!("{i} {colored}"));
             }
         }
     }
@@ -77,7 +82,8 @@ pub fn search_case_insensitive<'a>(queries: Vec<String>, contents: &'a str) -> V
         let query = query.to_lowercase();
         for (i, line) in contents.lines().enumerate() {
             if line.to_lowercase().contains(&query) {
-                results.push(format!("{i} {line}"));
+                let colored = colorize(&query, line);
+                results.push(format!("{i} {colored}"));
             }
         }
     }
